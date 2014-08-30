4b65ff85ad5f68050b63b4c6a2ccdd5cc248da7d
cys4 <33754977+entropyencoder@users.noreply.github.com>
2014-08-30 01:41:59 +0000

    - added an unaligned SAO filtering process test (not conforming to the standard) (enabled by setting 'TEST_UNALIGNED_SAO' to 1) - encoding process is not optimized for the new SAO filtering process.

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -43,6 +43,16 @@
 //! \ingroup TLibCommon
 //! \{
 
+#if 1     // added by yschoi
+
+#define TEST_UNALIGNED_SAO  1   // test a new SAO filtering to apply the parameters to post-deblocking recon. unaligned to CTU boundary
+#if TEST_UNALIGNED_SAO
+#define SAO_CTU_SHIFT_HOR   4
+#define SAO_CTU_SHIFT_VER   4
+#endif
+
+#endif    // added by yschoi
+
 // ====================================================================================================================
 // Debugging
 // ====================================================================================================================
